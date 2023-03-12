d597582293202de7bba4620978a5e952c5f0f589
KhanKhad <xxanov2017@mail.ru>
2023-03-12 19:59:58 +0300

    Добавьте файлы проекта.

---
+++ StartupsFront/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using StartupsFront.Models;
+using StartupsFront.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StartupsFront.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
